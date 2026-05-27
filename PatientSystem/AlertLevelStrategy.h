cfd272c6f338e86978c4176647b2ab61e9d33f70
ChhayZ7 <150762525+ChhayZ7@users.noreply.github.com>
2026-05-27 12:06:38 +0930

    Rename AlertStrategy to AlertLevelStrategy

--- PatientSystem/AlertLevelStrategy.h
+++ PatientSystem/AlertLevelStrategy.h
@@ -8,9 +8,9 @@ class  Vitals;
 
 // Strategy interface (FR3)
 // Each disease has its own algorithms for calculating an AlertLevel from a patient's vitals
-class AlertStrategy {
+class AlertLevelStrategy {
 public:
-	virtual ~AlertStrategy() = default;
+	virtual ~AlertLevelStrategy() = default;
 
 	// Calculate the alert level for a patient given a new vitals reading
 	virtual AlertLevel calculate(const Patient& patient, const Vitals& vitals) const = 0;
